14b1b434fbc4cc41e61b5347ef529662f7b2575e
Dima-Sosin <sosindima5@gmail.com>
2025-02-15 18:04:30 +0700

    Дописал все запросы по рецептам

--- routes/recipes.js
+++ routes/recipes.js
@@ -70,13 +70,30 @@ router.get("/card_recipes", authenticateToken, async (req, res) => {
 });
 
 // Получить всю инфу по конкретному рецепту
-router.get("/inf_recipe", async (req, res) => {
-    // try {
-    //   const recipes = await Recipe.find();
-    //   res.json(recipes);
-    // } catch (err) {
-    //   res.status(500).json({ message: err.message });
-    // }
+router.get("/inf_recipe/:id", authenticateToken, async (req, res) => {
+    try {
+        const recipe = await Recipe.findById(req.params.id).select("-creator -createdAt");
+
+        // Если рецепт не найден, возвращаем ошибку 404
+        if (!recipe) {
+            return res.status(404).json({ message: "Рецепт не найден" });
+        }
+
+        // Добавляем параметр like, если пользователь аутентифицирован
+        let recipeWithLike = recipe.toObject();
+        if (req.user) {
+            const user = await User.findById(req.user.id);
+            if (user) {
+                recipeWithLike.like = user.likedRecipes.includes(recipeId); // Проверка, лайкал ли пользователь рецепт
+            }
+        } else {
+            recipeWithLike.like = false; // Если пользователь не аутентифицирован, like: false
+        }
+
+        res.status(200).json(recipeWithLike);
+    } catch (err) {
+        res.status(500).json({ message: err.message });
+    }
 });
 
 // Создать новый рецепт
@@ -85,7 +102,7 @@ router.post("/add_recipe", authenticateToken, async (req, res) => {
         const { name, photos, cook_time, calories, ingredients, equipments, cook_steps } = req.body;
 
         if (!req.user) {
-            return res.status(401).json({ message: 'Токен отсутствует' });
+            return res.status(401).json({ message: "Токен отсутствует" });
         }
 
         const recipe = new Recipe({
@@ -102,6 +119,34 @@ router.post("/add_recipe", authenticateToken, async (req, res) => {
         await recipe.save();
 
         res.status(200).json({ message: "Рецепт сохранен" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+});
+
+// Добавить в избранное / убрать из избранного
+router.patch("/like_recipe/:id", authenticateToken, async (req, res) => {
+    try {
+        if (!req.user) {
+            return res.status(401).json({ message: "Токен отсутствует" });
+        }
+
+        const user = await User.findById(req.user.id);
+
+        // Проверяем, есть ли рецепт уже в избранном
+        const recipeIndex = user.liked_recipes.indexOf(req.params.id);
+
+        if (recipeIndex === -1) {
+            // Если рецепта нет в избранном, добавляем его
+            user.liked_recipes.push(req.params.id);
+            await user.save();
+            res.status(200).json({ message: "Рецепт добавлен в избранное" });
+        } else {
+            // Если рецепт уже в избранном, удаляем его
+            user.liked_recipes.splice(recipeIndex, 1);
+            await user.save();
+            res.status(200).json({ message: "Рецепт удалён из избранного" });
+        }
     } catch (error) {
         res.status(500).json({ message: error.message });
     }
